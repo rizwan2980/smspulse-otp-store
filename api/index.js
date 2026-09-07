57b40883ff7dc52819feeb3751d76c48ff57e5a3
rizwan2980 <rizwansaeed2980@gmail.com>
2026-09-07 05:03:03 -0700

    Bundle server files into Vercel function and add robust error reporting

--- api/index.js
+++ api/index.js
@@ -1,2 +1,17 @@
-const app = require('../server/server');
-module.exports = app;
+let app;
+
+module.exports = (req, res) => {
+  try {
+    if (!app) {
+      app = require('../server/server');
+    }
+    return app(req, res);
+  } catch (err) {
+    console.error('Vercel Serverless Error:', err);
+    res.status(500).json({
+      error: 'Vercel Serverless Error',
+      message: err.message,
+      stack: err.stack
+    });
+  }
+};
